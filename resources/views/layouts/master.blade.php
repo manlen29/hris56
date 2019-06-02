2954c96e4c27eefdfd6039d84e45702caa7a8fb2
manlen29 <manlen29@gmail.com>
2019-06-02 20:36:29 +0800

    Users Vue

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,18 +88,21 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
             <ul class="nav nav-treeview">
+              
               <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="fa fa-circle-o nav-icon"></i>
-                  <p>Active Page</p>
-                </a>
+                <router-link to="/users" class="nav-link">
+                <i class="fa fa-users" aria-hidden="true"></i>
+                  <p>Users</p>
+                </router-link>
               </li>
+
               <li class="nav-item">
                 <a href="#" class="nav-link">
                   <i class="fa fa-circle-o nav-icon"></i>
                   <p>Inactive Page</p>
                 </a>
               </li>
+
             </ul>
           </li>
           
@@ -113,12 +116,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-power-off"></i>
-              <p>
-                Logout
-              </p>
+            <a class="nav-link" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                document.getElementById('logout-form').submit();">
+               <i class="nav-icon fas fa-power-off"></i>  
+                <p>{{ __('Logout') }}</p>
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
           
         </ul>
